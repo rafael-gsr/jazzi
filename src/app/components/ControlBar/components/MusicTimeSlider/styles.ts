b5e88a49b4d69d0abfcfd7075b13e02aa91e4b75
rafael-gsr <rafaelgomes1404@gmail.com>
2024-06-14 20:40:26 -0300

    feat(ControlBar & components): changing styles and screen width conditional rendering

---
+++ src/app/components/ControlBar/components/MusicTimeSlider/styles.ts
@@ -0,0 +1,46 @@
+import * as colors from '@/app/styles/colors'
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  align-items:center;
+`
+
+export const SliderStyles = {
+  // cor da linha
+  color: 'white',
+
+  // cor do que já foi assistido
+  '& .MuiSlider-track': {
+    border: `2px solid ${colors.darkBlue[500]}`,
+  },
+
+  // bolinha
+  '& .MuiSlider-thumb': {
+    width: 18,
+    height: 18,
+    backgroundColor: `${colors.darkBlue[700]}`,
+    transition: 'all 0.1s',
+
+    // sombra em volta da bolinha
+    '&::before': {
+      boxShadow: `0 0px 2px ${colors.darkBlue[300]}`,
+    },
+
+    // estados de utilização da bolinha
+    '&:hover, &.Mui-active': {
+      boxShadow: `0 1px 10px ${colors.darkBlue[300]}`,
+      width: 24,
+      height: 24,
+    },
+
+    '&.Mui-focusVisible': {
+      boxShadow: 'none',
+    },
+  },
+}
+
+export const Numbers = styled.p`
+  margin: 0 16px;
+`
---
+++ src/app/components/ControlBar/components/VolumeControl/styles.ts
@@ -0,0 +1,52 @@
+import { grayscale } from "@/app/styles/colors";
+import styled from "styled-components";
+
+export const SliderStyle = {
+  width: '120px',
+
+  // cor da linha
+  color:'white',
+  
+  // cor do que já foi assistido
+  '& .MuiSlider-track': {
+    border: '2px solid white',
+  },
+
+  // bolinha 
+  '& .MuiSlider-thumb': {
+    width: 12,
+    height: 12,
+    backgroundColor: 'white',
+    transition: 'all 0.1s',
+
+    // sombra em volta da bolinha
+    '&::before': {
+      boxShadow: '0 0px 4px white',
+    },
+
+    // estados de utilização da bolinha
+    '&:hover, &.Mui-active': {
+      boxShadow: '0 0px 10px white',
+      width: 18,
+      height: 18,
+    },
+
+    '&.Mui-focusVisible': {
+      boxShadow: 'none'
+    }
+  }
+}
+
+export const DivPrincipal = styled.div`
+  display: flex;
+  flex-direction: row; 
+  align-items: center;
+  width: fit-content;
+
+  .icon{
+    width: 24px;
+    height: 24px;
+    margin: 0 16px;
+    cursor: pointer;
+  }
+`

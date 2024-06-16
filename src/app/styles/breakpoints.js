e7267afa0bc674919e511adce0a32d806e2802ff
rafael-gsr <rafaelgomes1404@gmail.com>
2024-06-16 15:41:37 -0300

    feat: creating a breakpoint files for styling

---
+++ src/app/styles/breakpoints.js
@@ -0,0 +1,8 @@
+export const breakpoints = {
+  SM: '(min-width: 360px)',
+  MD: '(min-width: 768px)',
+  LG: '(min-width: 1024px)',
+  XL: '(min-width: 1440px)',
+  XXL: '(min-width: 1600px)',
+  XXXL: '(min-width: 1920px)',
+}

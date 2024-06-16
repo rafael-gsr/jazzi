c1faef8c23fc905ac5afaed8b4ebf0c38794543e
rafael-gsr <rafaelgomes1404@gmail.com>
2024-06-16 15:41:13 -0300

    style(utils): changing the extensions

--- src/app/utils/useWidth/index.js
+++ src/app/utils/useWidth/index.js
@@ -1,9 +1,15 @@
+'use client'
 import React from "react"
 
 function useWidth () {
-  const [ currentWidth, setCurrentWidth ] = React.useState(window.screen.width)
-  window.onresize = () => setCurrentWidth(window.innerWidth)
-  window.onload = () => setCurrentWidth(window.innerWidth)
+  const [ currentWidth, setCurrentWidth ] = React.useState(360)
+
+  function handleCurrentWidth(event){
+    setCurrentWidth(event.currentTarget.innerWidth)
+  }
+
+  window.onresize = handleCurrentWidth
+  window.onload = handleCurrentWidth
 
   return currentWidth
 }

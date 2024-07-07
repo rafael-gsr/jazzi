19a042038c4d45a01584a0474249a10f22524b66
rafael-gsr <rafaelgomes1404@gmail.com>
2024-07-07 11:54:30 -0300

    refac(MusicContext): exposing a reducer function instead of a array with functions

--- src/app/context/musicContext/_index.tsx
+++ src/app/context/musicContext/_index.tsx
@@ -1,21 +1,50 @@
 'use client'
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 import * as Types from './types'
 
 const MusicContext = createContext({})
 
+function musicContextReducer (state:Types.MusicReducerStateProps, action: Types.MusicReducerActionProps) {
+  switch(action.type){
+    case Types.ReducerActionsTypes.NEXT:
+      if (state.playlistPos < state.playlist.length - 1) {
+        return { ...state, playlistPos: state.playlistPos + 1}
+      }
+      return state
+    case Types.ReducerActionsTypes.PREV:
+      if (state.playlistPos > 0) {
+        return { ...state, playlistPos: state.playlistPos - 1}
+      }
+      return state
+    case Types.ReducerActionsTypes.SETPLAYLIST:
+      return {
+        ...state,
+        playlist: action.payload as [],
+      }
+    case Types.ReducerActionsTypes.SETPLAYLISTPOS:
+      return {
+        ...state,
+        playlistPos: action.payload as number,
+      }
+    default:
+      throw new Error(
+        "The musicContextReducer must be used with an action parameter"
+      )
+  }
+}
+
 export const MusicContextProvider = ({children}: Types.IMusicContextProvider ) => {
-  const [ playlist, setPlaylist ] = useState<Types.Playlist>([])
-  const [ playlistPos, setPlaylistPos ] = useState<number>(0)
+  const [ musicContextState, dispatchMusicStateAction ] = useReducer(musicContextReducer , {
+    playlist: [],
+    playlistPos: 0,
+  })
 
   return(
-    <MusicContext.Provider value={{
-      playlist,
-      setPlaylist,
-      playlistPos,
-      setPlaylistPos
-    }}>
+    <MusicContext.Provider value={[
+      musicContextState,
+      dispatchMusicStateAction
+    ]}>
       {children}
     </MusicContext.Provider>
   )
--- src/app/context/musicContext/types.ts
+++ src/app/context/musicContext/types.ts
@@ -7,15 +7,33 @@ export type MusicContextProps = {
   thumb: string;
 }
 
-export type Playlist = MusicContextProps[]
+export type PlaylistProps = {
+  title: string;
+  musics: MusicContextProps[];
+}
 
 export type IMusicContextProvider = {
   children: React.ReactNode;
 }
 
-export type IMusicContextHook = {
-  playlist: Playlist;
-  setPlaylist: (value: Playlist) => void;
-  playlistPos: number;
-  setPlaylistPos: (value: number) => void;
+export type IMusicContextHook = [
+  MusicReducerStateProps,
+  (reduce: MusicReducerActionProps) => void,
+]
+
+export enum ReducerActionsTypes {
+  NEXT= 'NEXT',
+  PREV= 'PREV',
+  SETPLAYLIST= 'SETPLAYLIST',
+  SETPLAYLISTPOS= 'SETPLAYLISTPOS',
+}
+1
+export type MusicReducerActionProps = {
+  type: 'NEXT' | 'PREV' | 'SETPLAYLIST' | 'SETPLAYLISTPOS';
+  payload?: number | MusicContextProps[] | null | undefined;
+}
+
+export type MusicReducerStateProps = {
+  playlist: MusicContextProps[],
+  playlistPos: number,
 }
